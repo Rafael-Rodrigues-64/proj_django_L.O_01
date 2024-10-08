81ec30ae546d3ab04beb9ca111b7908e28158401
Rafael Rodrigues Gomes <rafarodrigs3@gmail.com>
2024-10-08 16:42:06 -0300

    lauch.json

---
+++ projeto_dj_LO/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for projeto_dj_LO project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_dj_LO.settings')
+
+application = get_asgi_application()
